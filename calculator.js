bb0a89d6a7fb15dd13a631b79333ac7fc144ef1c
madhuri <madhuri@openbrix.co.uk>
2022-09-21 17:27:46 +0530

    add function

---
+++ calculator.js
@@ -0,0 +1,64 @@
+function display(){
+    alert("display")
+}
+
+function c(){
+    alert("clear")
+}
+
+function plusmin(){
+    alert("+/-")
+}
+
+function per(){
+    alert("%")
+}
+function backslash(){
+    alert("/")
+}
+function seven(){
+    alert("7")
+}
+function eight()
+{
+    alert("8")
+}
+function nine(){
+    alert("9")
+}
+function multi(){
+    alert("*")
+}
+function four(){
+    alert("4")
+}
+function five(){
+    alert("5")
+}
+function six(){
+    alert("6")
+}
+function minus(){
+    alert("-")
+}
+function one(){
+    alert("1")
+}
+function two(){
+    alert("2")
+}
+function three(){
+    alert("3")
+}
+function plus(){
+    alert("+")
+}
+function zero(){
+    alert("0")
+}
+function dot(){
+    alert(".")
+}
+function equal(){
+    alert("=")
+}
